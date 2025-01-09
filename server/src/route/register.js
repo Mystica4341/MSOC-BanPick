732d8621f12bd23b5270383c1eddab23d7344ad2
FoxeiZ <57582539+FoxeiZ@users.noreply.github.com>
2025-01-09 20:59:27 +0700

    format to match space indent

--- server/src/route/register.js
+++ server/src/route/register.js
@@ -5,23 +5,23 @@
  * @returns {void}
  */
 export function registerController(socket, room) {
-    socket.join(room);
+  socket.join(room);
 
-    socket.emit('controller:connect', socket.id);
-    socket.on('disconnect', () => {
-        console.debug('Controller disconnected: ' + socket.id);
-        socket.to(room).emit('controller:disconnect', socket.id);
-    });
+  socket.emit('controller:connect', socket.id);
+  socket.on('disconnect', () => {
+    console.debug('Controller disconnected: ' + socket.id);
+    socket.to(room).emit('controller:disconnect', socket.id);
+  });
 
-    socket.onAny((event, ...args) => {
-        console.debug('Controller send event:', event, args);
-        socket.broadcast.to(room).emit(event, ...args);
-    });
+  socket.onAny((event, ...args) => {
+    console.debug('Controller send event:', event, args);
+    socket.broadcast.to(room).emit(event, ...args);
+  });
 
-    socket.onAnyOutgoing((event, ...args) => {
-        console.debug('Controller receive event:', event, args);
-        // do something? dunno yet
-    });
+  socket.onAnyOutgoing((event, ...args) => {
+    console.debug('Controller receive event:', event, args);
+    // do something? dunno yet
+  });
 }
 
 /**
@@ -30,21 +30,21 @@ export function registerController(socket, room) {
  * @returns {void}
  */
 export function registerDisplay(socket, room) {
-    socket.join(room);
+  socket.join(room);
 
-    socket.broadcast.to(room).emit('display:connect', socket.id);
-    socket.on('disconnect', () => {
-        console.debug('Display disconnected: ' + socket.id);
-        socket.to(room).emit('display:disconnect', socket.id);
-    })
+  socket.broadcast.to(room).emit('display:connect', socket.id);
+  socket.on('disconnect', () => {
+    console.debug('Display disconnected: ' + socket.id);
+    socket.to(room).emit('display:disconnect', socket.id);
+  })
 
-    socket.onAny((event, ...args) => {
-        console.debug('Display send event:', event, args);
-        socket.broadcast.to(room).emit(event, ...args);
-    });
+  socket.onAny((event, ...args) => {
+    console.debug('Display send event:', event, args);
+    socket.broadcast.to(room).emit(event, ...args);
+  });
 
-    socket.onAnyOutgoing((event, ...args) => {
-        console.debug('Display receive event:', event, args);
-        // do something? dunno yet
-    });
+  socket.onAnyOutgoing((event, ...args) => {
+    console.debug('Display receive event:', event, args);
+    // do something? dunno yet
+  });
 }
